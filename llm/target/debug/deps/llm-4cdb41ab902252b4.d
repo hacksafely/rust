cea47b3eb389b05f46734da8e0f5c7c59a619e58
hacksafely <jaykumar_soni@protonmail.com>
2023-11-27 19:21:20 +0100

    first commit kali

---
+++ llm/target/debug/deps/llm-4cdb41ab902252b4.d
@@ -0,0 +1,5 @@
+/home/jay/rust/git_rust_folder/rust/llm/target/debug/deps/llm-4cdb41ab902252b4.rmeta: src/main.rs
+
+/home/jay/rust/git_rust_folder/rust/llm/target/debug/deps/llm-4cdb41ab902252b4.d: src/main.rs
+
+src/main.rs:
---
+++ llm/target/debug/deps/llm-9580c7fe1da8f642.d
@@ -0,0 +1,5 @@
+/home/jay/rust/git_rust_folder/rust/llm/target/debug/deps/llm-9580c7fe1da8f642.rmeta: src/main.rs
+
+/home/jay/rust/git_rust_folder/rust/llm/target/debug/deps/llm-9580c7fe1da8f642.d: src/main.rs
+
+src/main.rs:
